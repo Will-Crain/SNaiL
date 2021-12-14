ad281f73cb1140e118f3b74149750998ce4b8170
EngineerYo <willcrain96@gmail.com>
2021-12-14 03:12:07 -0800

    Main loop updated

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-require('Room.js')
-require('Creep.js')
+require('require')
 
 module.exports.loop = function() {
-	//
+	for (let roomName in Game.rooms) {
+		let room = Game.rooms[roomName]
+		if (room.controller && room.controller.my) {
+			try {
+				room.run()
+
+			}
+			catch(e) {
+				console.log(e.stack || e)
+			}
+		}
+	}
 }
