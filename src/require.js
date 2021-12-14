fcf1e904bf01b2e8a397c907801f42fc18321ca0
EngineerYo <willcrain96@gmail.com>
2021-12-14 03:12:13 -0800

    Consolidated require calls

---
+++ src/require.js
@@ -0,0 +1,11 @@
+// Classes
+global.Task = require('Task')
+
+// Class extensions
+require('Creep')
+require('Room')
+require('RoomPosition')
+require('RoomObject')
+
+// Global junk
+require('globals')
