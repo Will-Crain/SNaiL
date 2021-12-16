c948195db0027f3d733bb4b77f77c8b193ed0538
EngineerYo <willcrain96@gmail.com>
2021-12-16 03:14:51 -0800

    Update to accommodate  Imperium

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 require('require')
 
 module.exports.loop = function() {
-	for (let roomName in Game.rooms) {
-		let room = Game.rooms[roomName]
-		if (room.controller && room.controller.my) {
-			try {
-				room.run()
-
-			}
-			catch(e) {
-				console.log(e.stack || e)
-			}
-		}
-	}
+	let Imperium = new IMPERIUM(Memory.Imperium || {})
+	
+	Imperium.load()
+	Imperium.run()
+	Imperium.save()
 }
