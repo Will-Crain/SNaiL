a5189c64b4dda3b7c5910cbcd6602b702e3e3075
EngineerYo <willcrain96@gmail.com>
2021-12-18 11:58:47 -0800

    Updated to reflect several new files

--- src/require.js
+++ src/require.js
@@ -3,11 +3,16 @@ global.IMPERIUM = require('Imperium')
 global.Sector = require('Sector')
 global.Task = require('Task')
 
+global.RoomGrid = require('RoomGrid')
+
 // Class extensions
 require('Creep')
+require('PathFinder')
 require('Room')
 require('RoomPosition')
 require('RoomObject')
+require('RoomVisual')
 
 // Global junk
 require('globals')
+require('misc')
