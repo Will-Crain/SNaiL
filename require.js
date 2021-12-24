1c6d1e3db88d32755eca11bc62328be2b784f573
EngineerYo <willcrain96@gmail.com>
2021-12-23 19:07:50 -0800

    Moved out of src for grunt-generated require file

---
+++ require.js
@@ -0,0 +1,17 @@
+// Classes
+global.IMPERIUM = require('Imperium')
+global.Sector = require('Sector')
+global.Task = require('Task')
+global.CreepBody = require('CreepBody')
+
+// Class extensions
+require('Creep')
+require('PathFinder')
+require('RoomPosition')
+require('RoomObject')
+require('RoomVisual')
+
+// Global junk
+require('globals')
+require('misc')
+require('hasRespawned')
