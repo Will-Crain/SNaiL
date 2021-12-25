970a3d5ddc78c4a954cb913242b255edb536d596
EngineerYo <willcrain96@gmail.com>
2021-12-24 18:40:41 -0800

    Refactored STATE_LOAD & STATE_UNLOAD; moved old versions to new depreciated file

---
+++ src/Class Extensions/Creep/STATE_LOAD.js
@@ -0,0 +1,53 @@
+Creep.prototype.STATE_LOAD = function(scope) {
+	let {loadArray, resource, canPop=true, unloadArray} = scope
+
+	// Are we already full?
+	if (this.store.getFreeCapacity(resource) == 0) {
+		this.memory.arraySpot = 0
+		if (unloadArray) {
+			this.pushState('UNLOAD', {unloadArray: unloadArray, loadArray: loadArray, resource: resource})
+		}
+		else if (canPop) {
+			this.popState()
+		}
+		else {
+			this.pushState('WAIT', {until: Game.time + 3})
+		}
+	}
+
+	if (_.isUndefined(this.memory.arraySpot)) {
+		this.memory.arraySpot = 0
+	}
+
+	let targetPosition = RoomPosition.parse(loadArray[this.memory.arraySpot])
+
+	if (!this.pos.inRangeTo(targetPosition, 1)) {
+		this.pushState('MOVE', {posStr: loadArray[this.memory.arraySpot]})
+		return
+	}
+	else {
+		// Check for structures
+		let validStructure = _.find(targetPosition.lookFor(LOOK_STRUCTURES), s => s.store && s.store.getUsedCapacity(resource) > 0)
+		if (validStructure) {
+			this.withdraw(validStructure, resource)
+			this.memory.arraySpot += 1
+			return
+		}
+
+		// Check for ground resources instead
+		let validResources = _.find(targetPosition.lookFor(LOOK_RESOURCES), s => s.resourceType == resource)
+		if (validResource) {
+			this.pickup(validResource)
+			this.memory.arraySpot += 1
+			return
+		}
+
+		// If neither, go to next position
+		this.memory.arraySpot += 1
+		if (this.memory.arraySpot >= loadArray.length) {
+			// Consider going back to STATE_UNLOAD here
+			this.memory.arraySpot = 0
+		}
+	}
+	
+}
---
+++ src/Class Extensions/Creep/STATE_UNLOAD.js
@@ -0,0 +1,54 @@
+Creep.prototype.STATE_UNLOAD = function(scope) {
+	let {unloadArray, resource, canPop=true, loadArray} = scope
+
+	// Are we already empty?
+	if (this.store.getUsedCapacity(resource) == 0) {
+		this.memory.arraySpot = 0
+	}
+	if (loadArray) {
+		this.pushState('LOAD', {loadArray: loadArray, unloadArray: unloadArray, resource: resource})
+	}
+	else if (canPop) {
+		this.popState()
+	}
+	else {
+		this.pushState('WAIT', {until: Game.time + 3})
+	}
+
+	if (_.isUndefined(this.memory.arraySpot)) {
+		this.memory.arraySpot = 0
+	}
+
+	let targetPosition = RoomPosition.parse(unloadArray[this.memory.arraySpot])
+
+	if (!this.pos.inRangeTo(targetPosition, 1)) {
+		this.pushState('MOVE', {posStr: unloadArray[this.memory.arraySpot]})
+		return
+	}
+	else {
+		// Check for structures first
+		let validStructure = _.find(targetPosition.lookFor(LOOK_STRUCTURES), s => s.store && s.store.getFreeCapacity(resource) > 0)
+		if (validStructure) {
+			this.transfer(validStructure, resource)
+			this.memory.arraySpot += 1
+			return
+		}
+
+		// Check for valid floor position
+		let validPosition = _.find(targetPosition.lookFor(LOOK_STRUCTURES), s => OBSTACLE_OBJECT_TYPES.includes(s.structureType))
+		if (!validPosition) {
+			this.moveTo(validPosition)
+			this.drop(resource)
+			this.memory.arraySpot += 1
+			return
+		}
+
+		// If neither, go to next position
+		this.memory.arraySpot += 1
+		if (this.memory.arraySpot >= unloadArray.length) {
+			// Consider going back to STATE_LOAD here
+			this.memory.arraySpot = 0
+		}
+	}
+
+}
