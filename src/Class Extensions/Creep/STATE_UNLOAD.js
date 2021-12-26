6b011383fe16e17ae122945288fff4cf6f9c0060
EngineerYo <willcrain96@gmail.com>
2021-12-26 10:36:42 -0800

    Removed a weird early popout spot

--- src/Class Extensions/Creep/STATE_UNLOAD.js
+++ src/Class Extensions/Creep/STATE_UNLOAD.js
@@ -2,18 +2,29 @@ Creep.prototype.STATE_UNLOAD = function(scope) {
 	let {unloadArray, resource, canPop=true, loadArray} = scope
 
 	// Are we already empty?
-	if (this.store.getUsedCapacity(resource) == 0) {
+	if (this.store[resource] == 0) {
 		this.memory.arraySpot = 0
+		if (loadArray) {
+			this.pushState('LOAD', {loadArray: loadArray, unloadArray: unloadArray, resource: resource})
+		}
+		else if (canPop) {
+			this.popState()
+		}
+		else {
+			this.pushState('WAIT', {until: Game.time + 3})
+		}
+		
+		return
 	}
-	if (loadArray) {
-		this.pushState('LOAD', {loadArray: loadArray, unloadArray: unloadArray, resource: resource})
-	}
-	else if (canPop) {
-		this.popState()
-	}
-	else {
-		this.pushState('WAIT', {until: Game.time + 3})
-	}
+	// if (loadArray) {
+	// 	this.pushState('LOAD', {loadArray: loadArray, unloadArray: unloadArray, resource: resource})
+	// }
+	// else if (canPop) {
+	// 	this.popState()
+	// }
+	// else {
+	// 	this.pushState('WAIT', {until: Game.time + 3})
+	// }
 
 	if (_.isUndefined(this.memory.arraySpot)) {
 		this.memory.arraySpot = 0
@@ -29,26 +40,32 @@ Creep.prototype.STATE_UNLOAD = function(scope) {
 		// Check for structures first
 		let validStructure = _.find(targetPosition.lookFor(LOOK_STRUCTURES), s => s.store && s.store.getFreeCapacity(resource) > 0)
 		if (validStructure) {
-			this.transfer(validStructure, resource)
+			this.say(this.transfer(validStructure, resource))
 			this.memory.arraySpot += 1
 			return
 		}
+		else {
 
-		// Check for valid floor position
-		let validPosition = _.find(targetPosition.lookFor(LOOK_STRUCTURES), s => OBSTACLE_OBJECT_TYPES.includes(s.structureType))
-		if (!validPosition) {
-			this.moveTo(validPosition)
-			this.drop(resource)
-			this.memory.arraySpot += 1
-			return
+			// Check for valid floor position
+			let validPosition = _.find(targetPosition.lookFor(LOOK_STRUCTURES), s => OBSTACLE_OBJECT_TYPES.includes(s.structureType))
+			if (!validPosition) {
+				this.moveTo(validPosition)
+				this.drop(resource)
+				this.memory.arraySpot += 1
+				return
+			}
+			else {
+				// If neither, go to next position
+				this.memory.arraySpot += 1
+				if (this.memory.arraySpot >= unloadArray.length) {
+					// Consider going back to STATE_LOAD here
+					this.memory.arraySpot = 0
+				}
+
+			}
+
 		}
 
-		// If neither, go to next position
-		this.memory.arraySpot += 1
-		if (this.memory.arraySpot >= unloadArray.length) {
-			// Consider going back to STATE_LOAD here
-			this.memory.arraySpot = 0
-		}
 	}
 
 }
