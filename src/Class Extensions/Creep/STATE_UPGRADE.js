508b40518e659068a596563e5dbb8575ee50d76e
EngineerYo <willcrain96@gmail.com>
2021-12-28 01:31:22 -0800

    Finally working

--- src/Class Extensions/Creep/STATE_UPGRADE.js
+++ src/Class Extensions/Creep/STATE_UPGRADE.js
@@ -3,23 +3,23 @@ Creep.prototype.STATE_UPGRADE = function(scope) {
 
 	let controller = Game.getObjectById(controllerID)
 	
-	// Are we empty?
-	if (this.store[RESOURCE_ENERGY] == 0) {
-		if (canPop) {
-			this.popState()
+	// Are we in our position?
+	let serializedPosition = RoomPosition.serialize(this.pos)
+	if (this.task.taskInfo.validPositions.includes(serializedPosition)) {
+		// Are we empty?
+		if (this.store[RESOURCE_ENERGY] == 0) {
+			if (canPop) {this.popState(); return}
+			
+			this.pushState('LOAD', {loadArray: loadFrom, canPop: true, resource: RESOURCE_ENERGY})
 		}
 		else {
-			this.pushState('LOAD', {loadArray: loadFrom, resource: RESOURCE_ENERGY})
+			this.upgradeController(controller)
 		}
 	}
-	// Otherwise, check if in range
-	else if (!this.task.taskInfo.validPositions.includes(RoomPosition.serialize(this.pos))) {
-		let positionMap = _.map(this.task.taskInfo.validPositions, s => RoomPosition.serialize(s))
-		let targetPosition = _.find(positionMap, s => s.lookFor(LOOK_CREEPS).length == 0)
-		this.pushState('MOVE', {posStr: RoomPosition.serialize(targetPosition), range: 0, errorPops: true})
-	}
 	else {
-		this.upgradeController(controller)
-		// add stats callback here?
+		this.say(':(')
+		let positionMap = _.map(this.task.taskInfo.validPositions, s => RoomPosition.parse(s))
+		let targetPosition = _.find(positionMap, s => !_.some(s.lookFor(LOOK_CREEPS), t => t.getState() != 'MOVE'))
+		this.pushState('MOVE', {posStr: RoomPosition.serialize(targetPosition), range: 0, errorPops: true})
 	}
 }
