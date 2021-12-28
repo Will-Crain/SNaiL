6dc557bf89b687b14a6ac7855a19b0dee3b1a14c
EngineerYo <willcrain96@gmail.com>
2021-12-28 01:30:56 -0800

    New optional parameter to allow creeps to move others in their path

--- src/Class Extensions/Creep/STATE_MOVE.js
+++ src/Class Extensions/Creep/STATE_MOVE.js
@@ -1,5 +1,5 @@
 Creep.prototype.STATE_MOVE = function(scope={}) {
-	let {posStr, range=1, ignoreCreeps=false, errorPops=false} = scope
+	let {posStr, range=1, ignoreCreeps=false, errorPops=false, canPush=false} = scope
 	let posObj = RoomPosition.parse(posStr)
 
 	if (this.room.name != posObj.roomName) {
@@ -19,12 +19,18 @@ Creep.prototype.STATE_MOVE = function(scope={}) {
 			
 			return
 		}
-		// Is there a stationary creep at our destination?
-		let obstructingCreep = _.find(posObj.lookFor(LOOK_CREEPS), s => s.my && s.getState() != 'MOVE')
-		if (!_.isUndefined(obstructingCreep) && errorPops) {
-			this.popState()
 
-			return
+		// Is there a stationary creep at our destination?
+		let obstructingCreep = _.find(posObj.lookFor(LOOK_CREEPS), s => s.my && (s.getState() != 'MOVE' || (s.memory._move && !s.memory._move.path)))
+		if (!_.isUndefined(obstructingCreep) && canPush && this.pos.getRangeTo(obstructingCreep.pos) == 1) {
+			obstructingCreep.cancelOrder('move')
+			obstructingCreep.move(obstructingCreep.pos.getDirectionTo(this.pos))
+			this.move(this.pos.getDirectionTo(obstructingCreep.pos))
+			this.say('>:(')
+			obstructingCreep.say(':(')
+		}
+		else if (!_.isUndefined(obstructingCreep) && errorPops) {
+			this.popState()
 		}
 	}
 }
